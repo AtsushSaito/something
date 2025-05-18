a137796acb7b33c67b46238f7b7a46399d25e380
Example User <example@example.com>
2025-05-19 03:05:37 +0900

    Implement cone subdivision algorithms for toric resolution of singularities

--- lakefile.lean
+++ lakefile.lean
@@ -9,3 +9,6 @@ package «something» {
 lean_lib «Something» {
   -- add library configuration options here
 }
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "v4.3.0-rc2"
